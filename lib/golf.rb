f27a73ce01359936b495c89c068eb49a491072f4
Lennon Rubin <lprubin.lennon@gmail.com>
2014-05-27 19:55:28 -0400

    added hold5

--- lib/golf.rb
+++ lib/golf.rb
@@ -11,5 +11,18 @@ class Golf
   def self.hole3(i)
     (1..i).inject &:*
   end
+  
+  def self.hole5 a
+    x = []
+    a.each do |i|
+      a.each_with_index do |n,i|
+        x << [n]
+        x << [n, a[i+1]] if i < 3
+        x << [n, a[i+1], a[i+2]] if i < 2
+      end
+    end
+    x << a
+    x.uniq.sort_by {|m| [m.length, m]}
+  end
 
 end
