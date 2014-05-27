9c04797485219c3fd9e7812feb5cc1859f361264
Tom Brown <tom@joingrouper.com>
2014-05-27 19:34:41 -0400

    Green number 6

--- lib/golf.rb
+++ lib/golf.rb
@@ -6,4 +6,18 @@ class Golf
   def self.hole3(i)
     (1..i).inject &:*
   end
+
+  def self.hole6(i)
+    (1..i).collect do |i|
+      if i % 15 == 0
+        "fizzbuzz"
+      elsif i % 3 == 0
+        "fizz"
+      elsif i % 5 == 0
+        "buzz"
+      else
+        i
+      end
+    end
+  end
 end
